73afac1930376e703875ad345b21186507fe2ebc
akhsa234 <saremi.bah2000@gmail.com>
2025-09-01 16:09:44 +0330

    first commit

--- kafka-producer-wikimedia/src/main/java/com/bahar/kafka/wikimedia/wikimediaChangeHandler.java
+++ kafka-producer-wikimedia/src/main/java/com/bahar/kafka/wikimedia/wikimediaChangeHandler.java
@@ -1,45 +1,51 @@
 package com.bahar.kafka.wikimedia;
 
-import com.launchdarkly.eventsource.MessageEvent;
 import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.beans.EventHandler;
 
+public class wikimediaChangeHandler implements EventHandler {
 
-public class wikimediaChangesHandler implements EventHandler {
-
-    KafkaProducer<String,String> kafkaProducer;
+    private final Logger log = LoggerFactory.getLogger(wikimediaChangeHandler.class.getSimpleName());
+    KafkaProducer<String, String> kafkaProducer;
     String topic;
 
-    public wikimediaChangesHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
+    public wikimediaChangeHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
         this.kafkaProducer = kafkaProducer;
         this.topic = topic;
     }
 
-    public void onOpen() throws java.lang.Exception{
+
+    @Override
+    public void onOpen() throws Exception {
+        //nothing
     }
 
-public void onClosed()
-        throws java.lang.Exception{
+    @Override
+    public void onClosed() {
+        kafkaProducer.close();
+    }
 
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent)  {
+        log.info(messageEvent.getData());
+        //async
+        kafkaProducer.send( new ProducerRecord<>( topic,messageEvent.getData()));
+            }
 
+    @Override
+    public void onComment(String comment)  {
+        //nothing
 
-}
-public void onMessage(java.lang.String event,
-                      MessageEvent messageEvent)
-        throws java.lang.Exception{
+    }
 
+    @Override
+    public void onError(Throwable t) {
+        log.info("error in stream reading" , t);
 
-}
-public void onComment(java.lang.String comment)
-        throws java.lang.Exception{
-
-}
-
-public void onError(java.lang.Throwable t){
-
-}
-
-
+    }
 }
--- kafka-producer-wikimedia/src/main/java/com/bahar/kafka/wikimedia/wikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/com/bahar/kafka/wikimedia/wikimediaChangesProducer.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class wikimediaChangesProducer {
+package com.bahar.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import com.launchdarkly.eventsource.*;
+
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class wikimediaChangesProducer {
+
+
+    public static void main(String[] args) {
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create producer peroperties
+        Properties properties = new Properties();
+
+        // connect to localhost
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        //set producer properties
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentChanges";
+
+        EventHandler eventHandler = new wikimediaChangeHandler(producer,topic);
+        String url = "https://stream.wikimedia.org/v2/stream.recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        // start the producer in another thread
+            eventSource.start();
+            // we produce for 10 minutes and block the program until then
+        try {
+            TimeUnit.MINUTES.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
